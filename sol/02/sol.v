4610d110739ee8baafa535de57bd89c92184191b
Igor Dimitrov <igor-dimi@protonmail.com>
2026-05-18 03:44:54 +0200

    add solutions sheet 3 ex 2

--- sol/02/sol.v
+++ sol/02/sol.v
@@ -17,7 +17,7 @@ Import ListNotations.
 Fixpoint length {A : Type} (l : list A): nat :=
     match l with
     | []        => 0
-    | x::xs     => 1 + length xs
+    | _::xs     => 1 + length xs
     end.
 
 Compute map length [[]; [1]; [1;2]; [1; 2; 3]]. (* a simple demo that it works*)
@@ -44,6 +44,51 @@ Compute suffix [1; 2; 3].
 (* Exercise 3 *)
 (* a *)
 
+Inductive EqBool : bool -> bool -> Set :=
+| tt : EqBool true true
+| ff : EqBool false false.
+
+Notation "b ≡ b'" := (EqBool b b') (at level 120, no associativity).
+
+Check tt.
+Check ff.
+
+Theorem negb_inv (b : bool) : negb (negb b) ≡ b.
+Proof.
+    destruct b.
+    - simpl. exact tt.
+    - simpl. exact ff.
+Qed.
+
+
+(* b *)
+Definition eqb : bool -> bool -> bool :=
+    fun b b' =>
+    match b, b' with
+    | true, true     => true
+    | false, false   => true
+    | _, _ => false
+    end.
+
+Notation "b == b'" := (eqb b b') (at level 120, no associativity).
+
+(* examples *)
+Compute eqb true true. 
+Compute eqb true false. 
+Compute eqb false true. 
+Compute eqb false false. 
+
+Compute true == true. 
+Compute true == false. 
+Compute false == true. 
+Compute false == false. 
+
+Theorem ex3b (b : bool) : (negb (negb b) == b) = true.
+Proof.
+    destruct b.
+    - simpl. reflexivity.
+    - simpl. reflexivity.
+Qed.
 
 
 
